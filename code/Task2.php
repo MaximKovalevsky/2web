d94484c3705bf35a6b219edc7b3267f5296df173
oddynuff <katemc19@mail.com>
2024-03-08 15:41:08 +0200

    Complete task2 + screen

---
+++ code/Task2.php
@@ -0,0 +1,21 @@
+<?php
+$digit1 = 24;
+
+echo $digit1;
+echo "<br>";
+echo "\n";
+
+$digit2 = 0.5;
+echo $digit2;
+echo "<br>";
+echo "\n";
+
+echo $digit1 * $digit2;
+
+echo "<br>";
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo $last_month - $this_month;
